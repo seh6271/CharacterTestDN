e2ed40b4f9ccc444d9b0aab5ef9ed7e73f809c85
shannon hanley <seh6271@g.rit.edu>
2017-11-20 17:38:15 -0500

    added The Boys

---
+++ ragdoll/Assets/Scripts/ActualWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualWalk : MonoBehaviour {
+
+    public Rigidbody pelvis;
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Motion();
+        }
+    }
+
+    void Motion()
+    {
+        pelvis.AddRelativeForce(new Vector3(0.0f, 0.0f, 1.0f) * walkSpeed);
+    }
+}
